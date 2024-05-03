f6d33055409fe7e903cbb9c6cdaa199361c9b6d3
Trieu Vi Le <101376216+trieuvi2032@users.noreply.github.com>
2024-05-02 19:24:32 -0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,6 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-  publicRoutes: [
-    "/",
-    "/events/:id",
-    "/api/webhook/clerk",
-    "/api/webhook/stripe",
-    "/api/webhook/uploadthing",
-  ],
-  ignoredRoutes: [
-    "/api/webhook/clerk",
-    "/api/webhook/stripe",
-    "/api/webhook/uploadthing",
-  ],
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
